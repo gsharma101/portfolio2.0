bc81017c9ffcfa0d518a28176dad9ef2d9fbcdd7
Gaurav Sharma <sharmag8121@gmail.com>
2021-10-11 23:53:01 +0530

    responsive setting

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'using strict';
+const ToggleIcon = document.getElementById('nav-toggle');
+const mobileSideNev = document.getElementById('mobile__nevigation');
+ToggleIcon.addEventListener('click',mobileNev);
+
+function mobileNev(){
+    
+}
