318a5b04439fc7e59628e7131cdf63d2315debd9
Gaurav Sharma <sharmag8121@gmail.com>
2021-10-12 20:02:13 +0530

    side nev complete final with animation

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@
 const ToggleIcon = document.getElementById('nav-toggle');
 const IconImage = document.getElementById('mobile__menu__image');
 const mobileSideNev = document.getElementById('mobile__nevigation');
+const nevItems = document.querySelectorAll('.mobile__nevigation--items');
+
 const mobileNev = function(){
   mobileSideNev.classList.toggle('width__display');
-
+  for(let i=0;i<nevItems.length;++i){
+    nevItems[i].classList.toggle('nevDisp');
+  }
   if(IconImage.classList.contains('bx-x-circle')){
    IconImage.classList.remove('bx-x-circle');
    IconImage.classList.add('bx-menu');
+   
   }else{
    IconImage.classList.add('bx-x-circle');
    IconImage.classList.remove('bx-menu');
