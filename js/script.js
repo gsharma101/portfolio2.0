301f7f89032d3f7bbfd0a25c3d550f173546f292
Gaurav Sharma <sharmag8121@gmail.com>
2021-10-12 16:17:56 +0530

    Added js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 'use strict';
 const ToggleIcon = document.getElementById('nav-toggle');
+const IconImage = document.getElementById('mobile__menu__image');
 const mobileSideNev = document.getElementById('mobile__nevigation');
 const mobileNev = function(){
-   if(mobileSideNev.style.width == 0)
-   {
-    mobileSideNev.style.width = '75%';  
-   }else{
-    mobileSideNev.style.width = 0;
-   }
+  mobileSideNev.classList.toggle('width__display');
+
+  if(IconImage.classList.contains('bx-x-circle')){
+   IconImage.classList.remove('bx-x-circle');
+   IconImage.classList.add('bx-menu');
+  }else{
+   IconImage.classList.add('bx-x-circle');
+   IconImage.classList.remove('bx-menu');
+  }
 }
 ToggleIcon.addEventListener('click', mobileNev);
