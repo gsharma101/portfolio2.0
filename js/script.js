695ac8918f0c48e2cfb88f75975e83dfb580a862
gsharma101 <sharmag8121@gmail.com>
2022-01-25 18:57:22 +0530

    updating the profile mobile slider

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ const ToggleIcon = document.getElementById('nav-toggle');
 const IconImage = document.getElementById('mobile__menu__image');
 const mobileSideNev = document.getElementById('mobile__nevigation');
 const nevItems = document.querySelectorAll('.mobile__nevigation--items');
+const nevLinks = document.querySelectorAll('.mobile__nevigation--links');
+
 
 const mobileNev = function(){
   mobileSideNev.classList.toggle('width__display');
   for(let i=0;i<nevItems.length;++i){
     nevItems[i].classList.toggle('nevDisp');
   }
+
   if(IconImage.classList.contains('bx-x-circle')){
    IconImage.classList.remove('bx-x-circle');
    IconImage.classList.add('bx-menu');
@@ -18,4 +21,9 @@ const mobileNev = function(){
    IconImage.classList.remove('bx-menu');
   }
 }
+
+for(let i = 0; i < nevLinks.length; ++i){
+    nevLinks[i].addEventListener('click',mobileNev);
+}
+
 ToggleIcon.addEventListener('click', mobileNev);
