5851e549b87bb4e6c2f378817d054feacffb90ad
Gaurav Sharma <56496704+gsharma101@users.noreply.github.com>
2022-06-20 01:00:39 +0530

    updading the theam mode btn

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,11 +1,22 @@
 'use strict';
 
-const lightBtn = document.querySelector('.circle');
+const lightBtn = document.querySelector('.sun');
 const body = document.querySelector('body');
+let val = 0;
+
 const darkMode = function(){
-    body.classList.toggle('body__change');
-    lightBtn.classList.toggle('circle__change');
+    if(val == 0)
+    {
+        lightBtn.setAttribute('src','images/icons/sun.svg');
+        body.style.backgroundColor = 'black';
+        body.style.transition = '1s';
+        ++val;
+    }else{
+        lightBtn.setAttribute('src','images/icons/moon.svg');
+        body.style.backgroundColor = 'white';
+        body.style.transition = '1s';
+        --val;
+    }
 }
 
 lightBtn.addEventListener('click', darkMode);
-
